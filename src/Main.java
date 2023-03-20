645275ad96b9eeaf189da016a92ee947464f63ed
Omar D. Ramirez <omardramirez2002@gmail.com>
2023-03-20 05:19:46 -0500

    adds readme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import layer1.layer2.test2;
+import layer1.test1;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        test1 layerValue = new test1();
+        test2 layer2Class = new test2();
+        System.out.println(layerValue.x);
+        System.out.println(layer2Class.addTenVal);
+
     }
 }
---
+++ src/layer1/layer2/test2.java
@@ -0,0 +1,8 @@
+package layer1.layer2;
+
+import layer1.test1;
+
+public class test2 {
+    test1 layer1class = new test1();
+    public int addTenVal = layer1class.x + 10;
+}
